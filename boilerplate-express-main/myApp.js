984676ac670f28eb45b51c6aa721ec1113b0ed41
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-02 10:31:20 -0300

    Basic Node and Express Module Finished

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
+let bodyParser = require("body-parser");
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -8,6 +9,8 @@ app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -20,7 +23,6 @@ app.get("/json", (req, res) => {
   }
   return res.status(200).json({ message: message });
 });
-
 
 app.get(
   "/now",
@@ -35,9 +37,21 @@ app.get(
   }
 );
 
-app.get("/:word/echo", (req,res)=>{
-  const word = req.params.word
- res.json({echo: word })
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
 });
+
+app.get("/name", (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
+app.post("/name", (req, res) => {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
+})
 
 module.exports = app;
