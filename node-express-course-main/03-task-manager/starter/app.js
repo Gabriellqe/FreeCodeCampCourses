78a3299a7b422bd0cae372d4df3f1c413bbcc3b3
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-02 12:10:58 -0300

    Update database asyncawit and .env config

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,14 +1,20 @@
-require("./db/connect.js")
 const express = require("express");
 const app = express();
 const tasks = require("./routes/task");
 const port = process.env.PORT || 5000;
-
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
 
 // Middleware
 app.use("/api/v1/tasks", tasks);
 
-// Routes
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-
-app.listen(port, console.log(`Listening on port ${port}`));
+start();
--- node-express-course-main/03-task-manager/starter/db/connect.js
+++ node-express-course-main/03-task-manager/starter/db/connect.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const connectionString =
-  "mongodb+srv://admin:Betel279@cluster0.gsjkuo2.mongodb.net/03-Task-Manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString,{
+const connectDB = (URL) => {
+  return mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {console.log("Connected to MongoDB");})
-  .catch((err) => console.log(err));
+    useFindAndModify: false,
+  });
+};
+
+module.exports = connectDB;
