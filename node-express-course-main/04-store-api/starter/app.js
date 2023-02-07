60d908343dad86ee1d18ed9b55f827a2d93779c0
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-07 12:30:34 -0300

    Finished store api

--- node-express-course-main/04-store-api/starter/app.js
+++ node-express-course-main/04-store-api/starter/app.js
@@ -1,1 +1,38 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store api!</h1> <a href="/api/v1/products"> products route</a');
+})
+app.use('/api/v1/products',productsRouter);
+
+//productos route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+        } catch (error) {
+            console.log(error);
+    }}
+
+start();
