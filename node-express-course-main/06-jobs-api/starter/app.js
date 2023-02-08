fbd60f54489ff3ac620798e57805e4b32ce89865
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-07 22:11:45 -0300

    Features in Register endPoint Validate name, email, password - with Mongoose, Hash Password (with bcryptjs), Save User Schema, Generate Token, Send Response with Token

--- node-express-course-main/06-jobs-api/starter/app.js
+++ node-express-course-main/06-jobs-api/starter/app.js
@@ -2,22 +2,18 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 //connect DB
 const connectDB = require("./db/connect")
 //Routers
 const jobsRouter = require('./routes/jobs');
 const authRouter = require('./routes/auth');
-
-
-
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
-
-
+app.use(morgan("dev"));
 app.use('/api/v1/jobs', jobsRouter);
 app.use('/api/v1/auth', authRouter);
 app.use(notFoundMiddleware);
--- node-express-course-main/06-jobs-api/starter/controllers/auth.js
+++ node-express-course-main/06-jobs-api/starter/controllers/auth.js
@@ -1,13 +1,23 @@
+const User = require("../models/user");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 
-const register = (req, res) => {
-  res.send('register');
-}
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ name: user.name , token });
+};
+
 const login = (req, res) => {
- res.send('register');
-}
-
+  res.send("register");
+};
 
 module.exports = {
- login,
- register
-}
+  login,
+  register,
+};
+
+/* if (!username || !email || !password) {
+    throw new BadRequestError("Please provide username, email and password");
+  } */
