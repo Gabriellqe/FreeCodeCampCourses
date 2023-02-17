e83e95a88f78a070e1aea17d1a4fb318e783395d
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-17 11:24:53 -0300

    Finished project from fundamentals lesson course

--- CourseReact18/my-app/src/index.js
+++ CourseReact18/my-app/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
-const Greeting = () => {
-return <h2>My first component</h2>
-}
+const BookList = () => {
+  return (
+    <>
+      <h1>Amazom best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          //const { author, title, img, id } = book; author={author} title={title} img={img}
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<Greeting />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
