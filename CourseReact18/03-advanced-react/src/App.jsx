37c32cf48ae6e09e6cd5cea0df5613050257b935
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-02-21 11:52:33 -0300

    finished useeffect lessons

--- CourseReact18/03-advanced-react/src/App.jsx
+++ CourseReact18/03-advanced-react/src/App.jsx
@@ -1,11 +1,10 @@
-import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
-import Final from "./tutorial/01-useState/final/05-useState-gotcha";
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import Final from "./tutorial/02-useEffect/final/01-code-example";
 
 function App() {
   return (
     <div className="container">
       <Starter />
-      {/*<Final />*/}
     </div>
   );
 }
